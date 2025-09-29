f8408330280226115ed772de0f66d9a1bb0bbbe5
Annette <notnatural21@gmail.com>
2025-09-29 15:37:57 +1300

    Create a HeaderBar component

---
+++ frontend/src/components/HeaderBar.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { Firebase } from '@/firebase';
+import { IonHeader, IonToolbar, IonTitle, IonIcon, IonButton } from '@ionic/vue';
+import { exitOutline } from 'ionicons/icons';
+defineProps({
+	name: String,
+});
+</script>
+
+<template>
+	<ion-header>
+		<ion-toolbar>
+			<ion-title>{{ name }}</ion-title>
+		</ion-toolbar>
+	</ion-header>
+</template>
